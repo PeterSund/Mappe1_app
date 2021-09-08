af659515a6725380fe1255332410c70377ce3e1e
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-08 13:55:26 +0200

    removed header, added Regnestykke class and edited game-activity layout

--- app/src/main/java/com/example/mappe1/GameActivity.java
+++ app/src/main/java/com/example/mappe1/GameActivity.java
@@ -1,4 +1,7 @@
 package com.example.mappe1;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 public class GameActivity {
 }
---
+++ app/src/main/java/com/example/mappe1/Regnestykke.java
@@ -0,0 +1,14 @@
+package com.example.mappe1;
+
+public class Regnestykke {
+    public String tall1;
+    public String tall2;
+    public int svar;
+
+    public Regnestykke (String tall1, String tall2, int svar){
+        this.svar = svar;
+        this.tall1 = tall1;
+        this.tall2 = tall2;
+    }
+}
+
