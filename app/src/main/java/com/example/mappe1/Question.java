75661ecf8995a6f9d4b215898ab8186471661a1b
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-23 14:15:09 +0200

    fixed images

--- app/src/main/java/com/example/mappe1/Question.java
+++ app/src/main/java/com/example/mappe1/Question.java
@@ -3,23 +3,30 @@ package com.example.mappe1;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+
+//Klasse for objectet Question
+//Klassen implementerer Parcelable for å kunne lagre gameQuestions<Question> arrayet når
+//onPause() blir kjørt under gameActivity.
 public class Question implements Parcelable {
     String question;
     String correctAnswer;
     boolean answeredCorrect;
 
+    //konstruktør for Questions-klassen
     public Question(String question, String correctAnswer, boolean answeredCorrect){
         this.correctAnswer = question;
         this.question = correctAnswer;
         this.answeredCorrect = answeredCorrect;
     }
 
+    //Metode som ble generert av Parcelable
     protected Question(Parcel in) {
         question = in.readString();
         correctAnswer = in.readString();
         answeredCorrect = in.readByte() != 0;
     }
 
+    //Metode som ble generert av Parcelable
     public static final Creator<Question> CREATOR = new Creator<Question>() {
         @Override
         public Question createFromParcel(Parcel in) {
@@ -32,6 +39,7 @@ public class Question implements Parcelable {
         }
     };
 
+    //Gettere og settere for Questions-klassen
     public String getQuestion() {
         return question;
     }
@@ -55,6 +63,8 @@ public class Question implements Parcelable {
     public void setAnsweredCorrect(boolean answeredCorrect) {
         this.answeredCorrect = answeredCorrect;
     }
+
+    //toString() som har blitt brukt for å teste innholdet av objektene i gameQuestions arrayet i gameActivity
     @Override
     public String toString() {
         return ("Q:"+this.getQuestion()+
@@ -62,11 +72,13 @@ public class Question implements Parcelable {
                 "\n AC: "+ this.isAnsweredCorrect());
     }
 
+    //Metode som ble generert av Parcelable
     @Override
     public int describeContents() {
         return 0;
     }
 
+    //Metode som ble generert av Parcelable
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(question);
